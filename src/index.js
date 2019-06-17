b2ee4c22acca5e3263380d364b5d23a1a9db5c9c
alechp <me@alechp.com>
2019-06-17 12:14:25 -0700

    package name and everything else is being returned as expected; npm link still broken

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 import path from "path";
+import chalk from "chalk";
 //TODO: Make this relative path entry via init function
-import { single, multiple } from "./sample.config";
 import { parser } from "./parser";
 import { linker } from "./linker";
+import pkgDir from "pkg-dir";
 const log = console.log;
 
 function init(szConfigFilePath, fnLinker) {
@@ -16,8 +17,14 @@ function init(szConfigFilePath, fnLinker) {
 
 //? This is only here for testing
 log(`single`);
-const szConfigFilePath = path.join(__dirname, "sample.config.js");
-init(szConfigFilePath, linker);
+//TODO: use pkgDir to get root pkgDir and use it as basis of finding sampleConfig
+//TODO: Revert sandbox loc because of chokidar
+pkgDir(__dirname).then(pkgDirPath => {
+  log(`${chalk.red("pkgDirPath")}: ${pkgDirPath}`);
+  const szConfigFilePath = path.join(pkgDirPath, "sample.config.js");
+  log(`${chalk.red("szConfigFilePath")}: ${szConfigFilePath}`);
+  init(szConfigFilePath, linker);
+});
 
 // log(`multiple`);
 // init(multiple, linker);
