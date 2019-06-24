c6c78ab35512c3985982748edc4f8d2ba67d85f0
alechp <me@alechp.com>
2019-06-24 10:41:13 -0700

    Removed pkg-dir & added imports to linker

---
+++ src/linker.js
@@ -0,0 +1,7 @@
+/*
+ ? Should the shell be async ? Since it's separated by a "cd" I assume it's okay
+ */
+const log = console.log;
+import shell from "shelljs";
+import chalk from "chalk";
+//TODO: Add watcher imports
